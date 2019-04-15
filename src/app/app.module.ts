20b10ac1b14645dc5202ca121e6693f4cf62e403
cristian <cristian.urrea4@gmail.com>
2019-04-15 11:31:35 +0200

    Componentes DropdownModule para todos los deplegables, y navbarModule (por hacer)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DropdownModule } from './searchModule/dropdown.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    DropdownModule,
     AppRoutingModule
   ],
   providers: [],
